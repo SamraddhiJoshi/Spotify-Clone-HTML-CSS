3517a0d7928f03088b9b0e059000d7b68607d9b4
SamraddhiJoshi <samraddhijoshi406@gmail.com>
2026-05-11 02:25:08 +0530

    Delete TYPESCRIPT directory

--- TYPESCRIPT/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Introduction to TypeScript - Case Study
-let userName = "Samraddhi Joshi";
-let age = 20;
-console.log("Hello " + userName);
-console.log("I am " + age + " years old");
